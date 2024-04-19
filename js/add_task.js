b9380b8321847d2818e257a829647ff7a152d052
Jonas2134 <neo.stief.jon@gmail.com>
2024-04-19 14:49:52 +0200

    add task first function

---
+++ js/add_task.js
@@ -0,0 +1,6 @@
+function createTask() {
+    let title = document.getElementById('taskTitle').value;
+    let discription = document.getElementById('taskDiscription').value;
+    let subtasks = document.getElementById('taskSubtasks').value;
+    let date = document.getElementById('taskDate').value;
+}
