e594e3f785a38165d24a314eab3bf3de6814bc34
Jonas2134 <neo.stief.jon@gmail.com>
2024-04-23 23:46:35 +0200

    Login, Signup js

---
+++ js/login-signup.js
@@ -0,0 +1,5 @@
+function addUser() {
+    let name = document.getElementById('user_name').value.trim();
+    let password = document.getElementById('user_email').value.trim();
+    let email = document.getElementById('user_password').value.trim();
+}
