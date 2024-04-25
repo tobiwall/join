8c978e1ae72ab0841ddfad4631cd29e53a95a8e3
tobiwall <t.w.95@web.de>
2024-04-25 22:49:57 +0200

    add hideUser function and fixed Bug show and hide User

--- js/add_task.js
+++ js/add_task.js
@@ -10,32 +10,32 @@ function initAddTask() {
 }
 
 function createTask(tasksColumn) {
-  let title = document.getElementById('taskTitle');
-  let discription = document.getElementById('taskDiscription');
-  let date = document.getElementById('taskDate');
-  let category = document.getElementById('categoryInput');
+  let title = document.getElementById("taskTitle");
+  let discription = document.getElementById("taskDiscription");
+  let date = document.getElementById("taskDate");
+  let category = document.getElementById("categoryInput");
 
   let newTask = {
-    'id': taskId++,
-    'title': title.value,
-    'description': discription.value,
-    'users': users,
-    'dueDate': date.value,
-    'prio': selectedPriority,
-    'category': category.value,
-    'subtasks': subtasks
+    id: taskId++,
+    title: title.value,
+    description: discription.value,
+    users: users,
+    dueDate: date.value,
+    prio: selectedPriority,
+    category: category.value,
+    subtasks: subtasks,
   };
 
   tasksColumn.push(newTask);
 
-  title.value = '';
-  discription.value = '';
-  date.value = '';
-  category.value = '';
-  selectedPriority = prioMedium('medium');
-  subtasks = [''];
+  title.value = "";
+  discription.value = "";
+  date.value = "";
+  category.value = "";
+  selectedPriority = prioMedium("medium");
+  subtasks = [""];
   subtaskId = 0;
-  users = [''];
+  users = [""];
 
   renderSubtasks();
   renderAssignedUser();
@@ -44,62 +44,71 @@ function createTask(tasksColumn) {
 }
 
 function clearAddTaskInput() {
-  document.getElementById('taskTitle').value = '';
-  document.getElementById('taskDiscription').value = '';
-  document.getElementById('taskDate').value = '';
-  document.getElementById('categoryInput').value = '';
-  document.getElementById('subtasksInput').value = '';
-  subtasks = [''];
+  document.getElementById("taskTitle").value = "";
+  document.getElementById("taskDiscription").value = "";
+  document.getElementById("taskDate").value = "";
+  document.getElementById("categoryInput").value = "";
+  document.getElementById("subtasksInput").value = "";
+  subtasks = [""];
   subtaskId = 0;
-  users = [''];
+  users = [""];
 
   renderSubtasks();
   renderAssignedUser();
 }
 
 function toggleButton(priority) {
-  if (priority === 'urgent') {
+  if (priority === "urgent") {
     prioUrgent(priority);
-  } else if (priority === 'medium') {
+  } else if (priority === "medium") {
     prioMedium(priority);
-  } else if (priority === 'low') {
+  } else if (priority === "low") {
     prioLow(priority);
   }
 }
 
 function prioUrgent(priority) {
-  document.getElementById(priority).classList.add('urgent-active');
-  document.getElementById(priority + 'Img').src = ('./assets/icons/prio_buttons/prio_urgent.png');
-  document.getElementById('medium').classList.remove('medium-active');
-  document.getElementById('mediumImg').src = ('./assets/icons/prio_buttons/prio_medium_yellow.png');
-  document.getElementById('low').classList.remove('low-active');
-  document.getElementById('lowImg').src = ('./assets/icons/prio_buttons/prio_low_green.png');
+  document.getElementById(priority).classList.add("urgent-active");
+  document.getElementById(priority + "Img").src =
+    "./assets/icons/prio_buttons/prio_urgent.png";
+  document.getElementById("medium").classList.remove("medium-active");
+  document.getElementById("mediumImg").src =
+    "./assets/icons/prio_buttons/prio_medium_yellow.png";
+  document.getElementById("low").classList.remove("low-active");
+  document.getElementById("lowImg").src =
+    "./assets/icons/prio_buttons/prio_low_green.png";
   selectedPriority = priority;
 }
 
 function prioMedium(priority) {
-  document.getElementById(priority).classList.add('medium-active');
-  document.getElementById(priority + 'Img').src = ('./assets/icons/prio_buttons/prio_medium.png');
-  document.getElementById('urgent').classList.remove('urgent-active');
-  document.getElementById('urgentImg').src = ('./assets/icons/prio_buttons/prio_urgent_red.png');
-  document.getElementById('low').classList.remove('low-active');
-  document.getElementById('lowImg').src = ('./assets/icons/prio_buttons/prio_low_green.png');
+  document.getElementById(priority).classList.add("medium-active");
+  document.getElementById(priority + "Img").src =
+    "./assets/icons/prio_buttons/prio_medium.png";
+  document.getElementById("urgent").classList.remove("urgent-active");
+  document.getElementById("urgentImg").src =
+    "./assets/icons/prio_buttons/prio_urgent_red.png";
+  document.getElementById("low").classList.remove("low-active");
+  document.getElementById("lowImg").src =
+    "./assets/icons/prio_buttons/prio_low_green.png";
   selectedPriority = priority;
 }
 
 function prioLow(priority) {
-  document.getElementById(priority).classList.add('low-active');
-  document.getElementById(priority + 'Img').src = ('./assets/icons/prio_buttons/prio_low.png');
-  document.getElementById('urgent').classList.remove('urgent-active');
-  document.getElementById('urgentImg').src = ('./assets/icons/prio_buttons/prio_urgent_red.png');
-  document.getElementById('medium').classList.remove('medium-active');
-  document.getElementById('mediumImg').src = ('./assets/icons/prio_buttons/prio_medium_yellow.png');
+  document.getElementById(priority).classList.add("low-active");
+  document.getElementById(priority + "Img").src =
+    "./assets/icons/prio_buttons/prio_low.png";
+  document.getElementById("urgent").classList.remove("urgent-active");
+  document.getElementById("urgentImg").src =
+    "./assets/icons/prio_buttons/prio_urgent_red.png";
+  document.getElementById("medium").classList.remove("medium-active");
+  document.getElementById("mediumImg").src =
+    "./assets/icons/prio_buttons/prio_medium_yellow.png";
   selectedPriority = priority;
 }
 
 function renderSubtasks() {
-  let subtasksList = document.getElementById('contentSubtasks');
-  subtasksList.innerHTML = '';
+  let subtasksList = document.getElementById("contentSubtasks");
+  subtasksList.innerHTML = "";
 
   for (let i = 0; i < subtasks.length; i++) {
     const task = subtasks[i];
@@ -108,13 +117,13 @@ function renderSubtasks() {
 }
 
 function addSubtask() {
-  let input = document.getElementById('subtasksInput');
+  let input = document.getElementById("subtasksInput");
   let subtaskText = input.value.trim();
-  
-  if (subtaskText !== '') {
+
+  if (subtaskText !== "") {
     subtasks.push(subtaskText);
     subtaskId++;
-    input.value = ''; 
+    input.value = "";
   }
   renderSubtasks();
 }
@@ -146,7 +155,7 @@ function editSubtask(task, i) {
 }
 
 function deleteSubtask(i) {
-  subtasks.splice(i, 1)
+  subtasks.splice(i, 1);
   renderSubtasks();
 }
 
@@ -154,15 +163,15 @@ function addChangedSubtask(i) {
   let input = document.getElementById(`changedSubtask${i}`);
   let subtaskText = input.value.trim();
   subtasks.splice(i, 1, subtaskText);
-  input.value = '';
+  input.value = "";
   renderSubtasks();
 }
 
 function clearInput() {
-  document.getElementById('taskTitle').value = '';
-  document.getElementById('taskDiscription').value = '';
-  document.getElementById('taskDate').value = '';
-  document.getElementById('categoryInput').value = '';
+  document.getElementById("taskTitle").value = "";
+  document.getElementById("taskDiscription").value = "";
+  document.getElementById("taskDate").value = "";
+  document.getElementById("categoryInput").value = "";
   subtasks = [];
 }
 
@@ -173,16 +182,31 @@ function loadContacts() {
   sortContacts();
 }
 
-function showUsers() {
-  let userList = document.getElementById('dropdown-users');
-  userList.innerHTML = '';
-  loadContacts();
+let assignedContainerClicked = false;
 
+function showUsers() {
+  let userList = document.getElementById("dropdown-users");
+  userList.innerHTML = "";
+  loadContacts();
+  if (!assignedContainerClicked) {
+    displayUserList(userList);
+  } else {
+    hideUsers();
+  }
+}
+
+function displayUserList(userList) {
+  assignedContainerClicked = true;
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
-    
     userList.innerHTML += userTemplate(contact);
   }
+}
+
+function hideUsers() {
+  assignedContainerClicked = false;
+  let userList = document.getElementById("dropdown-users");
+  userList.innerHTML = "";
 }
 
 //Close Funktion für Assigned USer funktioniert nicht...
@@ -203,13 +227,13 @@ document.addEventListener('click', function(event) {
 
 function handleCheckboxChange(event) {
   const checkbox = event.target;
-  const contactData = JSON.parse(checkbox.getAttribute('data-contact'));
+  const contactData = JSON.parse(checkbox.getAttribute("data-contact"));
 
   if (checkbox.checked) {
     users.push(contactData);
     renderAssignedUser();
   } else {
-    const index = users.findIndex(user => user.name === contactData.name);
+    const index = users.findIndex((user) => user.name === contactData.name);
     if (index !== -1) {
       users.splice(index, 1);
       renderAssignedUser();
@@ -229,33 +253,40 @@ function userTemplate(contact) {
       </div>
     </div>
     <div>
-      <input type="checkbox" name="assignedUser" value="${contact.name}" data-contact='${JSON.stringify(contact)}' onchange="handleCheckboxChange(event)">
+      <input type="checkbox" name="assignedUser" value="${
+        contact.name
+      }" data-contact='${JSON.stringify(
+    contact
+  )}' onchange="handleCheckboxChange(event)">
     </div>
   </div>
 `;
 }
 
 function searchUser() {
-  let searchValue = document.getElementById('userInput').value.trim().toLowerCase();
-  let userList = document.getElementById('dropdown-users');
-  userList.innerHTML = '';
+  let searchValue = document
+    .getElementById("userInput")
+    .value.trim()
+    .toLowerCase();
+  let userList = document.getElementById("dropdown-users");
+  userList.innerHTML = "";
   loadContacts();
 
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
-    if (((contact.name).trim().toLowerCase()).includes(searchValue)) {
+    if (contact.name.trim().toLowerCase().includes(searchValue)) {
       userList.innerHTML += userTemplate(contact);
     }
   }
 }
 
 function renderAssignedUser() {
-  let assignedUsers = document.getElementById('contentAssignedUsers');
-  assignedUsers.innerHTML = '';
+  let assignedUsers = document.getElementById("contentAssignedUsers");
+  assignedUsers.innerHTML = "";
 
   for (let i = 0; i < users.length; i++) {
     const user = users[i];
-    
+
     assignedUsers.innerHTML += `
     <div class="initialien-round-container" style="background-color: ${user.color};">
       ${user.initials}
@@ -264,15 +295,14 @@ function renderAssignedUser() {
   }
 }
 
-
 function save() {
   let toDoAsText = JSON.stringify(cardsToDo);
-  localStorage.setItem('toDos', toDoAsText);
+  localStorage.setItem("toDos", toDoAsText);
 }
 
 function load() {
-  let toDoAsText = localStorage.getItem('toDos');
-  if(toDoAsText) {
+  let toDoAsText = localStorage.getItem("toDos");
+  if (toDoAsText) {
     cardsToDo = JSON.parse(toDoAsText);
   }
 }
