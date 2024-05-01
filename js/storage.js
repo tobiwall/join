6e00385f1063ed0b83e657357e82ec7c1b0bfd14
Jonas2134 <neo.stief.jon@gmail.com>
2024-05-01 21:17:17 +0200

    login.js

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,8 @@
 const STORAGE_TOKEN = 'LBRCE7ZOJGJE5A61XE03E0RA3FUCZKJ11X9OKHSK';
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
+let currentUser = null;
+
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
