febcac24135538694df16d3c18d881a804676122
tobiwall <t.w.95@web.de>
2024-05-02 21:03:24 +0200

    filter function

--- js/board.js
+++ js/board.js
@@ -239,7 +239,7 @@ function toggleSubtask(i, j) {
 
 function generateCardHTML(task, i, taskId) {
   return /*html*/ `
-    <div draggable="true" ondragstart="startDragging(${task['id']})" class="card" id="card${task["id"]}" onclick="openTaskPopup(${i})">
+    <div draggable="true" ondragstart="startDragging(${task["id"]})" class="card" id="card${task["id"]}" onclick="openTaskPopup(${i})">
         <div class="small-card-category" id="category${i}_${taskId}">${task["category"]}</div>
         <h3>${task["title"]}</h3>
         <p>${task["description"]}</p>
@@ -454,9 +454,83 @@ function moveTo(statusContainer, index) {
 }
 
 function findTaskById() {
-  return allTasks.find(task => task.id === currentDraggedTask);
+  return allTasks.find((task) => task.id === currentDraggedTask);
 }
 
 function startDragging(id) {
   currentDraggedTask = id;
+}
+
+function filter() {
+  let searchInput = document.getElementById("searchInput");
+  let searchTerm = searchInput.value.toLowerCase();
+  filterTitle(searchTerm);
+}
+
+function filterTitle(search) {
+  let todo = document.getElementById("toDoContainer");
+  let progress = document.getElementById("inProgressContainer");
+  let feedback = document.getElementById("awaitFeedbackContainer");
+  let done = document.getElementById("doneContainer");
+
+  clearTaskContainer(todo, progress, feedback, done)
+  displayFilteredTasks(todo, progress, feedback, done, search);
+
+}
+
+function clearTaskContainer(todo, progress, feedback, done) {
+  todo.innerHTML = "";
+  progress.innerHTML = "";
+  feedback.innerHTML = "";
+  done.innerHTML = "";
+}
+
+function displayFilteredTasks(todo, progress, feedback, done, search) {
+  for (let i = 0; i < allTasks.length; i++) {
+    const task = allTasks[i];
+    const title = task.title;
+    const taskId = allTasks[i].id;
+    if (title.toLowerCase().includes(search)) {
+      filteredToDo(task, todo, i, taskId);
+      filteredInProgress(task, progress, i, taskId);
+      filteredAwaitFeedback(task, feedback, i, taskId);
+      filteredDone(task, done, i, taskId);
+    }
+  }
+}
+
+function filteredToDo(task, toDoContainer, i, taskId) {
+  if (task.status === "todo") {
+    toDoContainer.innerHTML += generateCardHTML(task, i, taskId);
+    categoryColor(i, taskId);
+    renderUsers(i, taskId);
+    generateCardPrio(task, i, taskId);
+  }
+}
+
+function filteredInProgress(task, inProgressContainer, i, taskId) {
+  if (task.status === "progress") {
+    inProgressContainer.innerHTML += generateCardHTML(task, i, taskId);
+    categoryColor(i, taskId);
+    renderUsers(i, taskId);
+    generateCardPrio(task, i, taskId);
+  }
+}
+
+function filteredAwaitFeedback(task, awaitFeedbackContainer, i, taskId) {
+  if (task.status === "feedback") {
+    awaitFeedbackContainer.innerHTML += generateCardHTML(task, i, taskId);
+    categoryColor(i, taskId);
+    renderUsers(i, taskId);
+    generateCardPrio(task, i, taskId);
+  }
+}
+
+function filteredDone(task, doneContainer, i, taskId) {
+  if (task.status === "done") {
+    doneContainer.innerHTML += generateCardHTML(task, i, taskId);
+    categoryColor(i, taskId);
+    renderUsers(i, taskId);
+    generateCardPrio(task, i, taskId);
+  }
 }
