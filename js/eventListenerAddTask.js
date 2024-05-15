a356031ee83d033b61310ec7dd3b6d7fafc491a1
tobiwall <t.w.95@web.de>
2024-05-15 12:34:09 +0200

    eventListener userList

--- js/eventListenerAddTask.js
+++ js/eventListenerAddTask.js
@@ -2,6 +2,7 @@ document.addEventListener("click", function(event) {
     const dropdowns = document.querySelectorAll(".dropdown");
     const userList = document.getElementById("editDropdownUsers");
     const addTaskUserList = document.getElementById("dropdown-users");
+    let categoryList = document.getElementById("dropdown-categories");
     const inputs = document.querySelectorAll("input");
     const userContainers = document.querySelectorAll(".user-container");
     const isClickInsideDropdown = Array.from(dropdowns).some(dropdown => dropdown.contains(event.target));
@@ -17,7 +18,11 @@ document.addEventListener("click", function(event) {
             }
             if (addTaskUserList) {
                 addTaskUserList.style.border = "0px";
+                addTaskUserList.innerHTML = "";
             }
+            if (categoryList) {
+                categoryList.style.border = "0px";
+                openCategories();            }
         });
     }
 });
