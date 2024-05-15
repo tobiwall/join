8cb63fa2e4357dad575bd543438bb826dcdbd8f5
tobiwall <t.w.95@web.de>
2024-05-15 19:54:46 +0200

    js docs contacts

--- js/contact.js
+++ js/contact.js
@@ -210,6 +210,10 @@ function randomBackgroundColor() {
   }
 }
 
+/**
+ * This function sort the array contacts by the alphabet
+ * 
+ */
 function orderFirstLetter() {
   for (let i = 0; i < contacts.length; i++) {
     let contact = contacts[i];
@@ -226,6 +230,11 @@ function clearFoundContacts() {
   foundContacts.length = 0;
 }
 
+/**
+ * This function sorts all contact by the first letter
+ * 
+ * @returns the array foundContacts
+ */
 function findContactsByFirstLetter() {
   for (let i = 0; i < firstLetterArray.length; i++) {
     let firstLetter = firstLetterArray[i];
@@ -243,6 +252,10 @@ function findContactsByFirstLetter() {
   return foundContacts;
 }
 
+/**
+ * renderContactListAlphabet shows the letters and calls the function to put them in
+ * 
+ */
 function renderContactListAlphabet() {
   let scrollableContainer = document.getElementById("scrollable-container");
   scrollableContainer.innerHTML = "";
@@ -255,6 +268,12 @@ function renderContactListAlphabet() {
   }
 }
 
+/**
+ * loadFoundContacts displays the contact in the container
+ * 
+ * @param {*} scrollableContainer is the div where the conacts are shown
+ * @param {*} foundcontact is the array of all sorted conacts
+ */
 function loadFoundContacts(scrollableContainer, foundcontact) {
   scrollableContainer.innerHTML += generateContactAlphabetListHTML(foundcontact);
     let alphabetListBox = document.getElementById(`alphabetList${foundcontact.letter}`);
@@ -265,12 +284,23 @@ function loadFoundContacts(scrollableContainer, foundcontact) {
       }
 }
 
+/**
+ * create the addContact button
+ * 
+ * @returns the html template
+ */
 function createButtonAddContact() {
   return /*html*/ `
     <button onclick="openAddContact()" class="add-new-contact-btn">Add new contact <img src="./assets/img/person_add.svg" alt=""></button>
   `;
 }
 
+/**
+ * generate the letters for the list
+ * 
+ * @param {*} foundcontact is the contact where to get the letter
+ * @returns the html template
+ */
 function generateContactAlphabetListHTML(foundcontact) {
   return /*html*/ `
         <div>
@@ -302,6 +332,12 @@ function generateContactListHTML(contact) {
     `;
 }
 
+/**
+ * findContactById search the contact width the given id
+ * 
+ * @param {*} id is the id of one contact we search
+ * @returns the found contact
+ */
 function findContactById(id) {
   for (let i = 0; i < contacts.length; i++) {
     let contact = contacts[i];
@@ -314,6 +350,11 @@ function findContactById(id) {
   return null;
 }
 
+/**
+ * This function open the contact and calls the function to show it in the right place
+ * 
+ * @param {*} id is the id of the clicked contact
+ */
 async function openContact(id) {
   let contactContainer = document.getElementById('contact-container');
   let klickedContact = findContactById(id);
@@ -330,6 +371,15 @@ async function openContact(id) {
   }
 }
 
+/**
+ * shows the contact box and call the functions to generate the html
+ * 
+ * @param {*} id of the contact from contacts
+ * @param {*} contactContainer is the container where the information is shown
+ * @param {*} klickedContact is the selected contact
+ * @param {*} contactBoxName is the div, where the headline is shown
+ * @param {*} contactInformation are the details of the contact
+ */
 async function showContactBox(id, contactContainer, klickedContact, contactBoxName, contactInformation) {
   document.body.style.overflowX = "hidden";
   contactContainer.style.animation = "";
@@ -342,306 +392,4 @@ async function showContactBox(id, contactContainer, klickedContact, contactBoxNa
   contactBoxName.innerHTML += generateContactBoxHTML(klickedContact, id);
   contactInformation.innerHTML = "";
   contactInformation.innerHTML += displayContactInfo(klickedContact);
-}
-
-function showClosingX() {
-  let xClosingContact = document.getElementById('xClosingContact');
-  if (contactMobile) {
-    xClosingContact.classList.remove('d-none')
-  } else {
-    xClosingContact.classList.add('d-none')
-  }
-}
-
-function closeContactContainer() {
-  let contactBox = document.getElementById('contactBox');
-  let scrollableContainer = document.getElementById('scrollable-container');
-  let xClosingContact = document.getElementById('xClosingContact');
-  contactBox.style.display = 'none';
-  scrollableContainer.style.display = 'flex';
-  xClosingContact.classList.add('d-none');
-}
-
-function closeContactBox() {
-  let contactBoxName = document.getElementById("contactBoxName");
-  let contactInformation = document.getElementById("contactInformation");
-  contactBoxName.innerHTML = "";
-  contactInformation.innerHTML = "";
-}
-
-function generateContactBoxHTML(contact, id) {
-  return /*html*/ `
-        <div 
-          class="initialien-big-round-container white-border" 
-          style="background-color: ${contact.color};">${contact.initials}
-        </div>
-        <div class="contact-box-name-container">
-          <p class="contact-box-name">${contact.name}</p>
-          <div class="contact-box-edit-delete">
-            <div class="edit-delete" id="edit" onclick="editContact(${id}, '${contact.color}', '${contact.initials}')">
-              <img class="img-black" src="./assets/img/edit.svg" alt="">
-              <img class="img-blue" src="./assets/icons/edit_blue.png" alt="">
-              <p>Edit</p>
-            </div>
-            <div class="edit-delete" onclick="deleteContact(${id})" id="delete">
-              <img class="img-black" src="./assets/img/delete.svg" alt="">
-              <img class="img-blue" src="./assets/icons/delete_blue.png" alt="">
-              <p>Delete</p>
-            </div>
-          </div>
-        </div>
-    `;
-}
-
-function displayContactInfo(klickedContact) {
-  return /*html*/ `
-    <div class="contact-information">
-      <p class="contact-info-headline">Contact Information</p>
-      <p class="bold">Email</p>
-      <p class="email">${klickedContact.email}</p>
-      <p class="bold">Phone</p>
-      <p class="phonenumber">${klickedContact.phone}</p>
-    </div>
-  `;
-}
-
-function openAddContact() {
-  let addContact = document.getElementById("addContact");
-  let overlay = document.getElementById("overlay");
-  addContact.style.right = "50%";
-  overlay.style.display = "block";
-  overlay.addEventListener("click", closeContactPopupByOverlay);
-}
-
-function closeContactPopup() {
-  clearInput();
-  let addContact = document.getElementById("addContact");
-  let editContact = document.getElementById("editContact");
-  let overlay = document.getElementById("overlay");
-  addContact.style.right = "-1200px";
-  editContact.style.right = "-1200px";
-  overlay.style.display = "none";
-}
-
-function closeContactPopupByOverlay(event) {
-  if (event.target.id === "overlay") {
-    closeContactPopup();
-  }
-}
-
-function saveContactsLocal() {
-  localStorage.setItem("contacts", JSON.stringify(contacts));
-}
-
-async function createContact() {
-  let nameInput = document.getElementById("nameInput");
-  let emailInput = document.getElementById("emailInput");
-  let phoneInput = document.getElementById("phoneInput");
-  await saveAndDisplayContacts(nameInput, emailInput, phoneInput);
-  addedContactAnimation();
-}
-
-function addedContactAnimation() {
-  let addedContactPopup = document.getElementById('addedContactPopup');
-  addedContactPopup.style.display = 'flex';
-  setTimeout(showContactPopupAndAnimate, 500);
-}
-
-function showContactPopupAndAnimate() {
-  let addedContactPopup = document.getElementById('addedContactPopup');
-
-  addedContactPopup.style.bottom = '50%';
-  setTimeout(() => {
-    addedContactPopup.style.bottom = '110%';
-  }, 1000);
-  setTimeout(() => {
-    addedContactPopup.style.display = 'none';
-    addedContactPopup.style.bottom = '-10%';
-  }, 1500);
-}
-
-async function saveAndDisplayContacts(nameInput, emailInput, phoneInput) {
-  let newContact = createNewContact(nameInput, emailInput, phoneInput);
-  contacts.push(newContact);
-  orderContacts();
-  displayContactsList();
-  let newContactId = newContact.id;
-  newContact = findContactById(newContactId);
-  await addContactToFirebase(newContact);
-  clearInput();
-  closeContactPopup();
-  renderContacts();
-}
-
-function findHighestId() {
-  let maxId = contacts[0].id;
-  for (let i = 1; i < contacts.length; i++) {
-    let contact = contacts[i];
-    if (contact !== null) {
-      if (contacts[i].id > maxId) {
-        maxId = contacts[i].id;
-      }
-    }
-  }
-  return maxId;
-}
-
-function createNewContact(nameInput, emailInput, phoneInput) {
-  if (phoneInput == null) {
-    phoneInput = "";
-    newBuildContacts = buildContact(nameInput, emailInput, phoneInput);
-  } else {
-    newBuildContacts = buildContact(nameInput, emailInput, phoneInput.value);
-  }
-  return newBuildContacts;
-}
-
-/**
- * buildContact builds a new contact
- * 
- * @param {*} nameInput is the text from the input
- * @param {*} emailInput is the text from the input
- * @param {*} phoneInput is the number from the input
- * @returns the new contact
- */
-function buildContact(nameInput, emailInput, phoneInput) {
-  lastContactId = findHighestId();
-  lastContactId++;
-  let newContact = {
-    name: nameInput.value,
-    email: emailInput.value,
-    phone: phoneInput,
-    id: lastContactId,
-  };
-  return newContact;
-}
-
-/**
- * clearInput cleares the input after closing a popup
- * 
- */
-function clearInput() {
-  let nameInput = document.getElementById("nameInput");
-  let emailInput = document.getElementById("emailInput");
-  let phoneInput = document.getElementById("phoneInput");
-  nameInput.value = "";
-  emailInput.value = "";
-  phoneInput.value = "";
-}
-
-/**
- * deteleContact search the right contact and delate it with splice and calls deleteData
- * 
- * @param {*} id is the index of the contact from contacts
- */
-function deleteContact(id) {
-  let openContact = findContactById(id);
-  let contactId = findIndexById(openContact);
-  let editContact = document.getElementById("editContact");
-  contacts.splice(contactId, 1);
-  deleteData(`/contacts/${id}`);
-  if (editContact.style.display !== "none") {
-    closeContactPopup();
-  }
-  closeContactBox();
-  renderContacts();
-}
-
-/**
- * findIndexById search the index of the contact
- * 
- * @param {*} openContact is the opened contact which you click
- * @returns the index of the openContact
- */
-function findIndexById(openContact) {
-  return contacts.findIndex((contact) => contact === openContact);
-}
-
-/**
- * editContact shows the popup and calls createInputValue
- * 
- * @param {*} id is the id from the contact
- * @param {*} color ist the backgroundcolor from the contact logo
- * @param {*} initials are the first letters from name and sirname
- */
-function editContact(id, color, initials) {
-  let editContact = document.getElementById("editContact");
-  let contactLogo = document.getElementById('contactLogo');
-  let overlay = document.getElementById("overlay");
-  editContact.style.right = "50%";
-  overlay.style.display = "block";
-  overlay.addEventListener("click", closeContactPopupByOverlay);
-  createInputValue(id);
-  contactLogo.style.backgroundColor = color;
-  contactLogo.innerHTML = /*html*/`
-    <div class="initialien-big-round-container">${initials}</div>
-  `
-}
-
-/**
- * createInputValue creates the new inputValue when you edit a contact
- * 
- * @param {*} id is the id from the contact
- */
-function createInputValue(id) {
-  let contact = findContactById(id);
-  let editNameInput = document.getElementById("editNameInput");
-  let editEmailInput = document.getElementById("editEmailInput");
-  let editPhoneInput = document.getElementById("editPhoneInput");
-  editNameInput.value = `${contact.name}`;
-  editEmailInput.value = `${contact.email}`;
-  editPhoneInput.value = `${contact.phone}`;
-  openEditContactId = contact.id;
-}
-
-/**
- * changeContact gets the inputvalue and call changeContactDetails to change them
- * 
- * @param {*} id is the id from the contact
- */
-function changeContact(id) {
-  let nameInput = document.getElementById("editNameInput").value;
-  let emailInput = document.getElementById("editEmailInput").value;
-  let phoneInput = document.getElementById("editPhoneInput").value;
-  changeContactDetails(nameInput, emailInput, phoneInput, id);
-}
-
-/**
- * changeContactDetails changes the details an put in the input value
- * 
- * @param {*} nameInput is the value of input name
- * @param {*} emailInput is the value of input email
- * @param {*} phoneInput is the value of input phone
- * @param {*} id is the id from the contact
- */
-async function changeContactDetails(nameInput, emailInput, phoneInput, id) {
-  let contact = findContactById(id);
-  contact.name = `${nameInput}`;
-  contact.email = `${emailInput}`;
-  contact.phone = `${phoneInput}`;
-  await updateContacts(contact, id);
-  renderContacts();
-  closeContactPopup();
-  openContact(id);
-}
-
-/**
- * checkContactIsMobile checks the with of the window and sets contactMobile = true or false
- * 
- */
-function checkContactIsMobile() {
-  let screenWidthContacts = window.innerWidth;
-  let contactBox = document.getElementById('contactBox');
-  let scrollableContainer = document.getElementById('scrollable-container');
-  let xClosingContact = document.getElementById('xClosingContact');
-  if (screenWidthContacts < 1351) {
-    contactMobile = true;
-    contactBox.style.display = 'none';
-    scrollableContainer.style.display = 'flex';
-    xClosingContact.classList.add('d-none')
-  } else {
-    contactMobile = false;
-    contactBox.style.display = 'flex';
-    showClosingX();
-  }
 }
