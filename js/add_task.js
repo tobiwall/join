5241e3f64099d383ee80da556b9a3fb932f48ea4
WladimirWagner <161317271+WladimirWagner@users.noreply.github.com>
2024-05-15 16:26:23 +0200

    fixing

--- js/add_task.js
+++ js/add_task.js
@@ -500,3 +500,19 @@ function hideCategories() {
   icon.style.transform = "rotate(0deg)";
   categoryList.innerHTML = "";
 }
+
+$(function(){
+  var dtToday = new Date();
+
+  var month = dtToday.getMonth() + 1;
+  var day = dtToday.getDate();
+  var year = dtToday.getFullYear();
+  if(month < 10)
+      month = '0' + month.toString();
+  if(day < 10)
+      day = '0' + day.toString();
+  
+  var maxDate = year + '-' + month + '-' + day;
+  alert(maxDate);
+  $('#taskDate').attr('min', maxDate);
+});
