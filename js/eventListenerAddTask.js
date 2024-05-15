7e35f3877a516f5c9a70eef0164f86f2bc133d66
tobiwall <t.w.95@web.de>
2024-05-15 12:09:39 +0200

    bug fixed eventListener userList

---
+++ js/eventListenerAddTask.js
@@ -0,0 +1,30 @@
+document.addEventListener("click", function(event) {
+    const dropdowns = document.querySelectorAll(".dropdown");
+    const userList = document.getElementById("editDropdownUsers");
+    const addTaskUserList = document.getElementById("dropdown-users");
+    const inputs = document.querySelectorAll("input");
+    const userContainers = document.querySelectorAll(".user-container");
+    const isClickInsideDropdown = Array.from(dropdowns).some(dropdown => dropdown.contains(event.target));
+    const isClickInsideInput = Array.from(inputs).some(input => input.contains(event.target));
+    const isClickInsideUserContainer = Array.from(userContainers).some(container => container.contains(event.target));
+  
+    if (!isClickInsideDropdown && !isClickInsideInput && !isClickInsideUserContainer) {
+        hideUsers();
+        dropdowns.forEach(dropdown => {
+            dropdown.style.border = "0px";
+            if (userList) {
+                userList.style.border = "0px";
+            }
+            if (addTaskUserList) {
+                addTaskUserList.style.border = "0px";
+            }
+        });
+    }
+});
+  
+const inputs = document.querySelectorAll("input");
+inputs.forEach(input => {
+    input.addEventListener("mousedown", function(event) {
+        event.stopPropagation();
+    });
+});
